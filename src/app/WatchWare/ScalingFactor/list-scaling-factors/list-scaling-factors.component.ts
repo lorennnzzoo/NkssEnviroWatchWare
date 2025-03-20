097e04cf660a8d733000ccbca23d0259fc49aa25
bhanu <lorenbhanu@gmail.com>
2025-03-20 16:29:03 +0530

    scaling factor deletion done

--- src/app/WatchWare/ScalingFactor/list-scaling-factors/list-scaling-factors.component.ts
+++ src/app/WatchWare/ScalingFactor/list-scaling-factors/list-scaling-factors.component.ts
@@ -7,15 +7,18 @@ import { InputIconModule } from 'primeng/inputicon';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { SelectModule } from 'primeng/select';
 import { CommonModule } from '@angular/common';
-import { ToastrService } from 'ngx-toastr';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ScalingFactorService } from '../../Services/scaling-factor.service';
 import { ScalingFactor } from '../../Interfaces/ScalingFactor';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 @Component({
   selector: 'app-list-scaling-factors',
-  imports: [TableModule, TagModule, IconFieldModule, InputTextModule, InputIconModule, MultiSelectModule, SelectModule, CommonModule],
+  imports: [TableModule, TagModule, IconFieldModule, InputTextModule, InputIconModule, MultiSelectModule, SelectModule, CommonModule, ToastrModule, ConfirmDialogModule],
   templateUrl: './list-scaling-factors.component.html',
-  styleUrl: './list-scaling-factors.component.css'
+  styleUrl: './list-scaling-factors.component.css',
+  providers: [ToastrService, ConfirmationService]
 })
 export class ListScalingFactorsComponent implements OnInit {
 
@@ -23,6 +26,7 @@ export class ListScalingFactorsComponent implements OnInit {
   Loading: boolean = false;
   constructor(
     private toastService: ToastrService,
+    private dialogService: ConfirmationService,
     private route: ActivatedRoute,
     private router: Router,
     private scalingFactorService: ScalingFactorService
@@ -57,6 +61,27 @@ export class ListScalingFactorsComponent implements OnInit {
     this.router.navigate(['/ScalingFactor/Edit', scalingFactor.Id])
   }
   onDelete(scalingFactor: ScalingFactor) {
+    this.dialogService.confirm({
+      message: 'Are you sure you want to delete this scaling factor?',
+      header: 'Confirm Deletion',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        // User clicked "Yes"
+        this.scalingFactorService.DeleteScalingFactor(scalingFactor).subscribe
+          ({
+            next: (response) => {
+              this.toastService.success('Scaling Factor deleted successfully', 'Deleted');
+              this.ngOnInit();
+            },
+            error: (error) => {
+              this.toastService.error(error.error, 'Unable To Delete Scaling Factor');
+            }
+          })
+      },
+      reject: () => {
+        // User clicked "No"
 
+      },
+    });
   }
 }
