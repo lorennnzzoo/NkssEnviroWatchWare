674ad9cd7add84c9eb6c7332c4e74cd1cf743eb7
bhanu <lorenbhanu@gmail.com>
2025-04-10 11:42:51 +0530

    updated virtual path for production apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts (for production)
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.146/' // Your API URL for production
+  apiUrl: 'http://192.168.0.146/NKSSAPI/' // Your API URL for production
 };
