248472ddb3afc976e9f95dfea0b6d6fe5a82f153
bhanu <lorenbhanu@gmail.com>
2025-04-11 12:12:26 +0530

    removed mail sent status

--- src/app/WatchWare/Interfaces/NotificationHistory.ts
+++ src/app/WatchWare/Interfaces/NotificationHistory.ts
@@ -3,12 +3,19 @@ import { ConditionType } from "./NotificationCondition";
 export interface NotificationHistory {
     Id: number;
     ChannelId: number;
+    StationId: number;
     ChannelName: string;
-    ConditionId: string;
-    ConditionType: ConditionType;
-    RaisedTime: string;
-    SentTime?: string | null;
+    StationName: string;
+    ConditionId: string; // GUIDs are represented as strings in TypeScript
+    ConditionType: string;
+    RaisedTime: string; // or Date if you convert
+    EmailSentTime: string | null;
+    SentEmailAddresses: string | null;
+    MobileSentTime: string | null;
+    SentMobileAddresses: string | null;
     Message: string;
     MetaData: string;
     IsRead: boolean;
 }
+
+
